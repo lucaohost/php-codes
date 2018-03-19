039d9604a94b2cbbe48afc9e61c62b22422fba51
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2018-03-19 01:00:35 -0300

    fila

--- Stack/Controler/services.stack.php
+++ Stack/Controler/services.stack.php
@@ -2,12 +2,21 @@
     require_once('../Model/Element.class.php');
     require_once('../Model/Stack.class.php');
     
-    public function addIntoStack($element){
-        if (is_empty(Stack::$rootElement)){ Stack::$rootElement = $element; }
-        //fazer recusão aqui
+    public function addIntoStack(c, $referenceElement = Stack::$rootElement){
+        if (empty(Stack::$rootElement)){
+            Stack::$rootElement = $element; 
+            return;    
+        }
+        if(empty($referenceElement->getNexPosition())){
+            $referenceElement->setNextElement($element);
+        }else{
+            addIntoStack($element,$referenceElement->getNexPosition());
+        }
     }
 
     public function removeFromStack(){
-        
+        $newRoot = Stack::$rootElement->getNextPostion();
+        unset($GLOBALS['$rootElement']);
+        Stack::$rootElement = $newRoot; 
     }
 ?>
