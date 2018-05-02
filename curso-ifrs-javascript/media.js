c38b681c043b7c11537469ae0d63c6d5ac0081c7
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2018-05-02 15:58:31 -0300

    Horas complementares, quero vcs

---
+++ curso-ifrs-javascript/media.js
@@ -0,0 +1,9 @@
+var nota1 = 8;
+var nota2 = 7;
+var nota3 = 10;
+var media = (nota1 + nota2 + nota3)/3;
+if(media >= 7){
+    alert("Media: "+media+" - Aprovado")
+}else{
+    alert("Reprovado");
+}
---
+++ curso-ifrs-javascript/meucodigo.js
@@ -0,0 +1,11 @@
+// var a = 2;
+var a = 12;
+var b = 3;
+var c = a+b;
+// alert(a+b);
+// alert(2+3);
+alert("Total da soma: " + c);
+
+var nome;
+nome = prompt("Informe o seu nome");
+alert("Olá, " + nome);
