69443851a7e88d3ad43ff9a60132670ab8deb2a4
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2018-05-04 15:58:41 -0300

    modulo2

---
+++ curso-ifrs-javascript/Modulo2/Conteudo/click.js
@@ -0,0 +1,8 @@
+function umClique(){
+    document.getElementById("imagem").src="fachada.jpg";
+    document.getElementById("log").innerHTML += "trocou para a foto da fachada<br>";
+}
+function doisCliques(){
+    document.getElementById("imagem").src="ifrs.gif";
+    document.getElementById("log").innerHTML+="trocou para o logo do IFRS-BG<br>"
+}
---
+++ curso-ifrs-javascript/Modulo2/Conteudo/input.js
@@ -0,0 +1,27 @@
+function foquei(){
+    
+}
+function saiCampo(){
+    
+}
+function selecionei(){
+
+}
+function mudei(){
+
+}
+function senteiODedo(){
+
+}
+function subiDedo(){
+
+}
+function x(){
+
+}
+function x(){
+
+}
+function x(){
+
+}
