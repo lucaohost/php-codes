19d1dcd2357e9791d6c86a55d54bbfbda80bdf89
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2018-05-05 19:34:48 -0300

    conteudo modulo 2

--- curso-ifrs-javascript/Modulo2/Conteudo/input.js
+++ curso-ifrs-javascript/Modulo2/Conteudo/input.js
@@ -1,27 +1,21 @@
 function foquei(){
-    
+    document.getElementById("log").innerHTML += 'eu sou daleste, cheguei<br>';
 }
 function saiCampo(){
-    
+    document.getElementById("log").innerHTML += 'mas já to saindo fora<br>';
 }
 function selecionei(){
-
+    document.getElementById("log").innerHTML += 'selecionei os brabo<br>';
 }
 function mudei(){
-
+    document.getElementById("log").innerHTML+='eu queria mudar, eu queria mudar<br>';
 }
 function senteiODedo(){
-
+    document.getElementById("log").innerHTML+='então senta o dedo nessa porra<br>';
 }
 function subiDedo(){
-
+    document.getElementById("log").innerHTML+='up up<br>';
 }
-function x(){
-
-}
-function x(){
-
-}
-function x(){
-
+function submeti(){
+    document.getElementById("log").innerHTML+='Submeti<br>';
 }
