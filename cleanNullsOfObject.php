9a1e1614e482bfbb0afe2d23b841c94b1eb7a589
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2019-05-01 19:03:05 -0300

    Update cleanNullsOfObject.php

--- cleanNullsOfObject.php
+++ cleanNullsOfObject.php
@@ -1,19 +1,22 @@
 <?php
-function cleanNullsOfObject(&$object) {
-    foreach ($object as $property => &$value) {
-        if (is_object($value)) {
-            self::cleanNullsOfObject($value);
-            if (empty(get_object_vars($value))) {
+class Helper{
+    
+    public static function cleanNullsOfObject(&$object) {
+        foreach ($object as $property => &$value) {
+            if (is_object($value)) {
+                self::cleanNullsOfObject($value);
+                if (empty(get_object_vars($value))) {
+                    unset($object->$property);
+                }
+            }
+            if(is_array($value) && is_object($value[0])){
+                foreach($value as $val){
+                    self::cleanNullsOfObject($val);
+                }
+            }
+            if (is_null($value) || (is_array($value) && empty($value))) {
                 unset($object->$property);
             }
-        }
-        if(is_array($value) && is_object($value[0])){
-            foreach($value as $val){
-                self::cleanNullsOfObject($val);
-            }
-        }
-        if (is_null($value) || (is_array($value) && empty($value))) {
-            unset($object->$property);
         }
     }
 }
